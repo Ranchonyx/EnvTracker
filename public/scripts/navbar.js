c57743ecf7165681da5a7781dfd2d5f22f63ffce
Ranchonyx <felixjanetzki@gmail.com>
2025-01-27 20:31:08 +0100

    navbar entry for analysis page

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -5,7 +5,17 @@ function VizButtonClickHandler(ev) {
     window.location.href = `/viz/${guid}`;
 }
 
+function AnalysisButtonClickHandler(ev) {
+    ev.preventDefault();
+
+    const guid = sessionStorage.getItem("last-station");
+    window.location.href = `/analysis/${guid}`;
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
     const vizButton = document.querySelector("a.visualisation");
     vizButton.addEventListener("click", VizButtonClickHandler);
+
+    const analysisButton = document.querySelector("a.analysis");
+    analysisButton.addEventListener("click", AnalysisButtonClickHandler);
 });
