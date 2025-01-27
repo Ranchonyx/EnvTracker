66e9bedf4f56e6ab720be10210ae94a578dc4521
Ranchonyx <felixjanetzki@gmail.com>
2025-01-27 22:35:59 +0100

    added crop recommendations patch 1.3

--- src/Routes/crop.route.ts
+++ src/Routes/crop.route.ts
@@ -5,13 +5,17 @@ import Service from "../Services/CropService/crop.service.js";
 const router = express.Router();
 
 router.post("/:station_id/recommendCrops", async (req, res) => {
-	const brief: QueryStationStatusResponse = req.body;
+	const brief: Array<Measurement<"Temperature" | "Humidity" | "Pressure", "°C" | "hPa" | "%">> = req.body;
 	const cropService = Service.GetInstance();
 
+	const temperature = brief.find(b => b.name = "Temperature");
+	const humidity = brief.find(b => b.name = "Humidity");
+	const pressure = brief.find(b => b.name = "Pressure");
+
 	const recommendedCrops = cropService.RecommendCropsFor({
-		temperature: parseFloat(brief.Temperature+""),
-		humidity: parseFloat(brief.Humidity+""),
-		pressure: parseFloat(brief.Pressure+"")
+		temperature: parseFloat(temperature + ""),
+		humidity: parseFloat(humidity + ""),
+		pressure: parseFloat(pressure + "")
 	});
 
 	res.send(recommendedCrops);
