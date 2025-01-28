77fe202a7e6fd0c26965966f3be0d4d4f5aaddac
Ranchonyx <felixjanetzki@gmail.com>
2025-01-28 08:48:23 +0100

    added HOUR_AND_MINUTE, HOUR, MINUTE groupings

--- public/scripts/viz.js
+++ public/scripts/viz.js
@@ -26,7 +26,7 @@ function getStationId() {
     return sessionStorage.getItem("last-station");
 }
 
-async function RenderChartAndDisplays(type) {
+async function RenderChartAndDisplays(type = localStorage.getItem("last-type")) {
     const today = new Date().toISOString();
 
     const dateFromElement = document.querySelector("#date-from");
@@ -34,10 +34,11 @@ async function RenderChartAndDisplays(type) {
 
     const shouldUseDateFrame = dateFromElement.value.length > 0 && dateToElement.value.length > 0;
 
-    const params = shouldUseDateFrame ? `from=${new Date(dateFromElement.value).toISOString()}&to=${new Date(dateToElement.value).toISOString()}` : `forDay=${today}`;
+    const timeFrameParams = shouldUseDateFrame ? `from=${new Date(dateFromElement.value).toISOString()}&to=${new Date(dateToElement.value).toISOString()}` : `forDay=${today}`;
+    const groupingParams = `&groupBy=${localStorage.getItem("last-group") || "HOUR_AND_MINUTE"}`;
 
     const myStationId = getStationId();
-    const typeDataRequest = await fetch(`/measurement/${getStationId()}/${type}?${params}`);
+    const typeDataRequest = await fetch(`/measurement/${getStationId()}/${type}?${timeFrameParams}${groupingParams}`);
     const typeData = await typeDataRequest.json();
 
     const chartDataResponse = await fetch(`/chart/${myStationId}/transform`, {
@@ -93,26 +94,13 @@ async function QueryAvailableMeasurementTypes() {
     return request.json();
 }
 
-async function entryClickEventListener(ev) {
-    ev.preventDefault();
-    const typeSpecifier = ev.target.textContent;
-
-    const targetText = `Messwerte (${typeSpecifier})`;
-
-    localStorage.setItem("last-type", typeSpecifier);
-
-    document.querySelector(".dropdown-toggle").textContent = targetText;
-
-    await RenderChartAndDisplays(typeSpecifier);
-}
-
 function createDropdownEntry(name) {
     const anchor = document.createElement("a");
     anchor.classList.add("dropdown-item");
     anchor.href = "#";
     anchor.innerText = name;
 
-    anchor.addEventListener("click", entryClickEventListener);
+    anchor.addEventListener("click", dropDownTypeClickListener);
 
     return anchor;
 }
@@ -134,6 +122,40 @@ async function SetupTypeDropdown() {
     dropdownMenu.append(...dropdownEntries);
 }
 
+
+async function dropDownTypeClickListener(ev) {
+    ev.preventDefault();
+    const typeSpecifier = ev.target.textContent;
+
+    const targetText = `${typeSpecifier}`;
+
+    localStorage.setItem("last-type", typeSpecifier);
+
+    document.querySelector(".dropdown-types-toggle").textContent = targetText;
+
+    await RenderChartAndDisplays();
+}
+
+async function dropDownGroupCliclListener(ev) {
+    ev.preventDefault();
+    const groupSpecifier = ev.target.textContent;
+
+    const targetText = `${groupSpecifier}`;
+
+    localStorage.setItem("last-group", groupSpecifier);
+
+    document.querySelector(".dropdown-groups-toggle").textContent = targetText;
+
+    await RenderChartAndDisplays();
+}
+
+function SetupGroupDropdown() {
+    const dropdownMenu = document.querySelector(".dropdown-menu-groups");
+    dropdownMenu.querySelectorAll("a.dropdown-item").forEach(entry => {
+        entry.addEventListener("click", dropDownGroupCliclListener);
+    })
+}
+
 document.addEventListener("DOMContentLoaded", async (ev) => {
     ev.preventDefault();
 
@@ -143,6 +165,7 @@ document.addEventListener("DOMContentLoaded", async (ev) => {
     sc.Send(`subscribe#new-record-${getStationId()}`);
 
     await SetupTypeDropdown();
+    SetupGroupDropdown();
 
     localStorage.removeItem("last-type");
     globalThis.ChartRenderer = chartRenderer("#sensorChart");
